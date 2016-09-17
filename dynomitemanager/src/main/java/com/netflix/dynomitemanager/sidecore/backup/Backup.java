e476efdebadb57b8e797962807b564b471ef8d0f
Ioannis Papapanagiotou <iduckhd@hotmail.com>
2016-09-17 11:56:14 -0700

    Code cleaning prior to ARDB changes

--- dynomitemanager/src/main/java/com/netflix/dynomitemanager/sidecore/backup/Backup.java
+++ dynomitemanager/src/main/java/com/netflix/dynomitemanager/sidecore/backup/Backup.java
@@ -16,8 +16,6 @@ import java.io.File;
 
 import org.joda.time.DateTime;
 
-import com.netflix.dynomitemanager.sidecore.ICredential;
-
 public interface Backup {
 	boolean upload(File file, DateTime todayStart);
 }
