7006fda7bf2f8d81fbb76b4bc59f616307fe2579
Ioannis Papapanagiotou <iduckhd@hotmail.com>
2016-09-19 12:50:43 -0700

    cleaning

--- dynomitemanager/src/main/java/com/netflix/dynomitemanager/sidecore/aws/AwsRoleAssumptionCredential.java
+++ dynomitemanager/src/main/java/com/netflix/dynomitemanager/sidecore/aws/AwsRoleAssumptionCredential.java
@@ -12,12 +12,8 @@
  */
 package com.netflix.dynomitemanager.sidecore.aws;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
 import com.google.inject.Inject;
 import com.netflix.dynomitemanager.defaultimpl.IConfiguration;
@@ -25,66 +21,69 @@ import com.netflix.dynomitemanager.identity.InstanceEnvIdentity;
 import com.netflix.dynomitemanager.sidecore.ICredential;
 
 public class AwsRoleAssumptionCredential implements ICredential {
-	private static final String AWS_ROLE_ASSUMPTION_SESSION_NAME = "AwsRoleAssumptionSession";
-	private ICredential cred;
-	private IConfiguration config;
-	private InstanceEnvIdentity insEnvIdentity;
-	private AWSCredentialsProvider stsSessionCredentialsProvider;
+    private static final String AWS_ROLE_ASSUMPTION_SESSION_NAME = "AwsRoleAssumptionSession";
+    private ICredential cred;
+    private IConfiguration config;
+    private InstanceEnvIdentity insEnvIdentity;
+    private AWSCredentialsProvider stsSessionCredentialsProvider;
 
-	@Inject
-	public AwsRoleAssumptionCredential(ICredential cred, IConfiguration config,
-			InstanceEnvIdentity insEnvIdentity) {
-		this.cred = cred;
-		this.config = config;
-		this.insEnvIdentity = insEnvIdentity;
-	}
+    @Inject
+    public AwsRoleAssumptionCredential(ICredential cred, IConfiguration config, InstanceEnvIdentity insEnvIdentity) {
+	this.cred = cred;
+	this.config = config;
+	this.insEnvIdentity = insEnvIdentity;
+    }
 
-	@Override
-	public AWSCredentialsProvider getAwsCredentialProvider() {
-		if (this.config.isDualAccount() || this.stsSessionCredentialsProvider == null) {
-			synchronized (this) {
-				if (this.stsSessionCredentialsProvider == null) {
+    @Override
+    public AWSCredentialsProvider getAwsCredentialProvider() {
+	if (this.config.isDualAccount() || this.stsSessionCredentialsProvider == null) {
+	    synchronized (this) {
+		if (this.stsSessionCredentialsProvider == null) {
 
-					String roleArn = null;
-					/**
-					 *  Create the assumed IAM role based on the environment.
-					 *  For example, if the current environment is VPC,
-					 *  then the assumed role is for EC2 classic, and vice versa.
-					 */
-					if (this.insEnvIdentity.isClassic()) {
-						// Env is EC2 classic --> IAM assumed role for VPC created
-						roleArn = this.config.getVpcAWSRoleAssumptionArn();
-					} else {
-						// Env is VPC --> IAM assumed role for EC2 classic created
-						roleArn = this.config.getClassicAWSRoleAssumptionArn();
-					}
+		    String roleArn = null;
+		    /**
+		     * Create the assumed IAM role based on the environment. For
+		     * example, if the current environment is VPC, then the
+		     * assumed role is for EC2 classic, and vice versa.
+		     */
+		    if (this.insEnvIdentity.isClassic()) {
+			// Env is EC2 classic --> IAM assumed role for VPC
+			// created
+			roleArn = this.config.getVpcAWSRoleAssumptionArn();
+		    } else {
+			// Env is VPC --> IAM assumed role for EC2 classic
+			// created
+			roleArn = this.config.getClassicAWSRoleAssumptionArn();
+		    }
 
-					//
-					if (roleArn == null || roleArn.isEmpty())
-						throw new NullPointerException(
-								"Role ARN is null or empty probably due to missing config entry");
+		    //
+		    if (roleArn == null || roleArn.isEmpty())
+			throw new NullPointerException(
+				"Role ARN is null or empty probably due to missing config entry");
 
-					/**
-					 *  Get handle to an implementation that uses AWS Security Token Service (STS) to create temporary,
-					 *  short-lived session with explicit refresh for session/token expiration.
-					 */
-					try {
-						this.stsSessionCredentialsProvider = new STSAssumeRoleSessionCredentialsProvider(
-								this.cred.getAwsCredentialProvider(), roleArn,
-								AWS_ROLE_ASSUMPTION_SESSION_NAME);
+		    /**
+		     * Get handle to an implementation that uses AWS Security
+		     * Token Service (STS) to create temporary, short-lived
+		     * session with explicit refresh for session/token
+		     * expiration.
+		     */
+		    try {
+			this.stsSessionCredentialsProvider = new STSAssumeRoleSessionCredentialsProvider(
+				this.cred.getAwsCredentialProvider(), roleArn, AWS_ROLE_ASSUMPTION_SESSION_NAME);
 
-					} catch (Exception ex) {
-						throw new IllegalStateException(
-								"Exception in getting handle to AWS Security Token Service (STS).  Msg: "
-										+ ex.getLocalizedMessage(), ex);
-					}
+		    } catch (Exception ex) {
+			throw new IllegalStateException(
+				"Exception in getting handle to AWS Security Token Service (STS).  Msg: "
+					+ ex.getLocalizedMessage(),
+				ex);
+		    }
 
-				}
-
-			}
 		}
 
-		return this.stsSessionCredentialsProvider;
-
+	    }
 	}
+
+	return this.stsSessionCredentialsProvider;
+
+    }
 }
